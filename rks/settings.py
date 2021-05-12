cbb6f203ee869ca43c2172941689980468cce875
Rohit <rohitkumarsingh1947@gmail.com>
2021-05-13 00:56:16 +0530

    changes in setting.py/host

--- rks/settings.py
+++ rks/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '48&*qp)n5f%vls$8k0(eg72iixcmu8oxm#8lu+j0j4tb=j)ldx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
